fd6ec0d40284998a22fcd1ebcf9a8761734c86f9
NetchanokY <tungpang.yindee2548@gmail.com>
2026-02-06 16:05:37 +0700

    create base demo navbar

--- cn_planner/lib/core/widgets/bottom_nav_bar.dart
+++ cn_planner/lib/core/widgets/bottom_nav_bar.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+class BottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const BottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Roadmap'),
+        BottomNavigationBarItem(icon: Icon(Icons.schedule), label: 'Schedule'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+      ],
+    );
+  }
+}
--- cn_planner/lib/main.dart
+++ cn_planner/lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
-import 'features/home/presentation/home_page.dart';
-import 'features/roadmap/presentation/roadmap_page.dart';
-import 'features/schedule/presentation/schedule_page.dart';
-import 'features/profile/presentation/profile_page.dart';
+import 'app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: ProfilePage());
-  }
 }
