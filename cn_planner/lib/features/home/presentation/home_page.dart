833fdbd5224a0176c8d87ad2cbeaaf54bf7e0826
NetchanokY <tungpang.yindee2548@gmail.com>
2026-02-06 15:44:44 +0700

    add 4 demo base pages

---
+++ cn_planner/lib/features/home/presentation/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Home Page',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            Text(
+              'Welcome to CN-Planner',
+              style: TextStyle(fontSize: 16, color: Colors.grey.shade700),
+            ),
+
+            const SizedBox(height: 30),
+
+            ElevatedButton(
+              onPressed: () {
+                debugPrint('Demo button pressed!');
+              },
+              child: const Text('Demo button'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
