6169ee0fdb10be2fcf2f69d28e5c2c8208b203ab
NetchanokY <tungpang.yindee2548@gmail.com>
2026-02-09 13:06:47 +0700

    merge nav_bar into main

--- cn_planner_app/test/widget_test.dart
+++ cn_planner_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cn_planner/main.dart';
+import 'package:cn_planner_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
