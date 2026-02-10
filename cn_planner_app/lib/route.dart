f276ec569770532e2f096adace2641381fb9f615
Thanawan Phongphaew <6610685171@cn103>
2026-02-10 20:23:46 +0700

    Add gpa on route

--- cn_planner_app/lib/route.dart
+++ cn_planner_app/lib/route.dart
@@ -4,6 +4,7 @@ import 'features/auth/views/register_page.dart';
 import 'features/profile/views/profile_page.dart';
 import 'features/profile/views/edit_profile_page.dart';
 import 'features/profile/views/setting_page.dart';
+import 'features/gpa_calculator/presentation/gpa_calculator_page.dart';
 
 class AppRoutes {
   static const login = '/login';
@@ -11,6 +12,7 @@ class AppRoutes {
   static const profile = '/profile';
   static const edit_profile = '/edit_profile';
   static const setting = '/setting';
+  static const gpa = '/gpa';
 
   static final routes = {
     login: (context) => const LoginPage(),
@@ -18,5 +20,6 @@ class AppRoutes {
     profile: (context) => ProfilePage(),
     edit_profile: (context) => EditProfilePage(),
     setting: (context) => const SettingPage(),
+    gpa: (context) => const GPACalculatorPage(),
   };
 }
