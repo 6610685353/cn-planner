ea908703311495728395c52fce7028fd514fd3fe
Punnawat Namkum <6610685247@student.tu.ac.th>
2026-02-13 21:04:07 +0700

    Create base structure in manage course page

--- cn_planner_app/lib/features/manage/views/manage_course_page.dart
+++ cn_planner_app/lib/features/manage/views/manage_course_page.dart
@@ -1,16 +1,54 @@
 import 'package:cn_planner_app/core/constants/app_colors.dart';
+import 'package:cn_planner_app/features/manage/widgets/search_box.dart';
+import 'package:cn_planner_app/features/manage/widgets/year_course.dart';
 import 'package:flutter/material.dart';
 
-class ManageCoursePage extends StatelessWidget{
+class ManageCoursePage extends StatelessWidget {
   const ManageCoursePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.background,
-      body: Text(
-        'Test Manage'
+      appBar: AppBar(
+        backgroundColor: AppColors.background,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.maybePop(context),
+        ),
+        title: const Text('Manage Course')
+      ),
+      body: Column(
+        children: [
+          SearchBox(),
+          Expanded(
+            child: ListView(
+              children: [
+                YearCourseBox(),
+                YearCourseBox(),
+                YearCourseBox(),
+                YearCourseBox(),
+                YearCourseBox(),
+                YearCourseBox(),
+                YearCourseBox(),
+                YearCourseBox(),
+              ],
+            ))
+        ],
       )
+      ,
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size.fromHeight(50), // ปุ่มกว้างเต็มหน้าจอ
+            backgroundColor: Colors.blue,
+          ),
+          child: const Text('Confirm', style: TextStyle(color: Colors.white)),
+        ),
+      ),
     );
   }
 }
