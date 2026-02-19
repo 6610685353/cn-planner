c39e79557e922fb32f3d56764552d1a46b8ce3ec
Punnawat Namkum <6610685247@student.tu.ac.th>
2026-02-20 01:18:58 +0700

    Successfully connect frontback Supabase

--- cn_planner_app/lib/services/data_fetch.dart
+++ cn_planner_app/lib/services/data_fetch.dart
@@ -1,4 +1,6 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class DataFetch {
   static final DataFetch _instance = DataFetch._internal();
@@ -29,5 +31,32 @@ class DataFetch {
     } catch (e) {
       throw Exception('Error fetching data, Error message: $e');
     }
+  }
+
+  Future<List<dynamic>> fetchEnrolled(String uid) async {
+    print("UID: $uid");
+    final url = Uri.parse("http://192.168.1.198:3000/api/v1/enrolled/$uid");
+
+    try {
+      print("3 in fetch");
+      final response = await http.get(
+        url,
+        headers: {"Content-Type": "application/json"}
+      ).timeout(Duration(seconds: 5));
+
+
+      print("Status: ${response.statusCode}");
+      print("BODY: ${response.body}");
+      return jsonDecode(response.body);
+    } catch (e) {
+      print("Error: $e");
+      return [];
+    }
+
+    // if (response.statusCode == 200) {
+    //   
+    // } else {
+    //   throw Exception("Failed to load data");
+    // }
   }
 }
