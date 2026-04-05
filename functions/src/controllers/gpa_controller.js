d9997d288221bb0083d1d31a525bfefb641f9f14
Punnawat Namkum <6610685247@student.tu.ac.th>
2026-04-06 01:19:03 +0700

    complete GPA calc logic

--- functions/src/controllers/gpa_controller.js
+++ functions/src/controllers/gpa_controller.js
@@ -1,13 +1,32 @@
 const gpaService = require("../services/gpa_service");
 
-exports.getGPA = async (req, res, next) => {
+const getGPA = async (req, res, next) => {
   try {
-    const { uid } = req.params;
+    const { uid } = req.query;
 
-    const result = await gpaService.calculateGPA(uid);
+    const result = await gpaService.getGPA(uid);
 
     res.status(200).json(result);
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    console.log(`Error getGPA : ${err}`)
+    next(err);
   }
 };
+
+const getThisSem = async (req, res, next) => {
+  try {
+    const { uid } = req.query;
+    
+    const result = await gpaService.getThisSem(uid);
+
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(`Error getThisSem : ${err}`);
+    next(err);
+  }
+}
+
+module.exports = {
+  getGPA,
+  getThisSem,
+}
---
+++ functions/src/models/gpa_model.js
@@ -0,0 +1,31 @@
+const getSupabase = require('../config/supabase');
+
+async function getGPAcred(uid) {
+    const supabase = getSupabase();
+    const { data, error } = await supabase
+        .from('profiles')
+        .select('gpax, earned_credits, gpa, this_sem_credits')
+        .eq("user_id", uid);
+
+    if(error) throw error;
+
+    return data;
+}
+
+async function getThisSem(uid) {
+    const supabase = getSupabase();
+    const { data, error } = await supabase
+        .from('UserRoadmap')
+        .select(`year, semester, Subjects (subjectCode, subjectName, credits)`)
+        .eq("user_id", uid)
+        .eq("status", "planned")
+
+    if (error) throw error;
+
+    return data;
+} 
+
+module.exports = {
+    getGPAcred,
+    getThisSem,
+}
