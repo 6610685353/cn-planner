f76a6da27f1eaa2847f0bbd0abae3374fd43e97d
Siranat P <6610685353@student.tu.ac.th>
2026-04-23 22:52:58 +0700

    fix test folder

--- cn_planner_app/test/widget_test.dart
+++ cn_planner_app/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:cn_planner_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(startRoute: '/login'));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
